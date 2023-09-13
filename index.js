7954cdab1451c9dab58b44ffadc6832c954c747b
kane <kane.stokes717@gmail.com>
2023-09-13 12:00:19 -0500

    new button started

--- index.js
+++ index.js
@@ -6,27 +6,23 @@ console.log('My code is running');
 // aesthetically pleasing (aka be styled)
 
 // Create a variable that will hold your to-do list items.
-let todo = document.querySelector("#list1");
+let todo = [];
 
-// Add a function that takes in a string as input and adds it to the to-do list.
-const addInput = (input) => {
-  todo.innerHTML += input;
-}
-
-// Add an event handler to populate an HTML element with your to-do list when the user clicks the appropriate button.
-const btn = document.getElementById('btn1');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
 const list = document.getElementById("list1");
 let inputthing = '';
-
-btn.addEventListener('click', function master(){
-  let input1 = document.querySelector('#input1').addEventListener(
-    'input',
-    (eventObject) => {
-      inputthing = eventObject.target.value;
-      // console.log(eventObject.target.value)
-    }
-  );
-  list.innerHTML += "<input type = 'checkbox'>"+inputthing+"</input><br>";
+// Add a function that takes in a string as input and adds it to the to-do list.
+let inputListener = document.querySelector('#input1').addEventListener(
+  'input',
+  (eventObject) => inputthing = eventObject.target.value
+)
+btn1.addEventListener('click', function addInput(){
+  // Add an event handler to populate an HTML element with your to-do list when the user clicks the appropriate button.
+  list.innerHTML += "<input type = 'checkbox' class = 'strikethrough'></input><label for ='strikethrough'>"+inputthing+"</label><br>";
 });
+btn2.addEventListener('click', function remove_(){
+  
+})
 
 // Etc... break down the big problems into baby steps and solve them systematically. Test as you go.
